acd52dc0fa4419594dd566ddb9cf0dd3b95b3ca9
Nikola Zivojinovic <zivojinovicn13@gmail.com>
2024-07-13 16:58:29 +0200

    phonebook-backend exercises 3.15-3.18

--- index.js
+++ index.js
@@ -7,12 +7,10 @@ const Phonebook = require('./models/phonebook');
 const app = express();
 
 app.use(cors());
+app.use(express.static('dist'));
 
 // Middleware to parse JSON body
 app.use(express.json());
-
-// Custom token to log request body
-morgan.token('req-body', (req, res) => JSON.stringify(req.body));
 
 // Middleware to log request time
 const logRequestTime = (req, res, next) => {
@@ -20,6 +18,10 @@ const logRequestTime = (req, res, next) => {
 	// console.log(`Request recived at: ${req.requestTime}`);
 	next();
 };
+app.use(logRequestTime);
+
+// Custom token to log request body
+morgan.token('req-body', (req, res) => JSON.stringify(req.body));
 
 // Middleware to log POST request with req body
 const logPOSTRequest = (req, res, next) => {
@@ -30,74 +32,74 @@ const logPOSTRequest = (req, res, next) => {
 		next();
 	}
 };
-
 app.use(morgan('tiny'));
-
-app.use(logRequestTime);
 app.use(logPOSTRequest);
 
-app.use(express.static('dist'));
+const unknownEndpoint = (req, res) => {
+	res.status(404).send({ error: 'unknown endpoint' });
+};
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-	console.error(err.stack);
-	res.status(500).send('Something broke!');
-});
+// let phoneBook = [
+// 	{
+// 		id: '1',
+// 		name: 'Arto Hellas',
+// 		number: '040-123456'
+// 	},
+// 	{
+// 		id: '2',
+// 		name: 'Ada Lovelace',
+// 		number: '39-44-5323523'
+// 	},
+// 	{
+// 		id: '3',
+// 		name: 'Dan Abramov',
+// 		number: '12-43-234345'
+// 	},
+// 	{
+// 		id: '4',
+// 		name: 'Mary Poppendieck',
+// 		number: '39-23-6423122'
+// 	}
+// ];
 
-let phoneBook = [
-	{
-		id: '1',
-		name: 'Arto Hellas',
-		number: '040-123456'
-	},
-	{
-		id: '2',
-		name: 'Ada Lovelace',
-		number: '39-44-5323523'
-	},
-	{
-		id: '3',
-		name: 'Dan Abramov',
-		number: '12-43-234345'
-	},
-	{
-		id: '4',
-		name: 'Mary Poppendieck',
-		number: '39-23-6423122'
-	}
-];
-
-app.get('/info', (req, res) => {
+app.get('/info', (req, res, next) => {
 	const reqRecivedTime = req.requestTime;
-	const numOfPeople = phoneBook.length;
-	res.send(`
-        <p>Phonebook has info for ${numOfPeople} people</p>
-        <br/>
-        <p>${reqRecivedTime}</p>
-    `);
+	Phonebook.find({})
+		.then((phoneBooks) => {
+			res.send(`
+	    <p>Phonebook has info for ${phoneBooks.length} people</p>
+	    <br/>
+	    <p>${reqRecivedTime}</p>`);
+		})
+		.catch((error) => next(error));
 });
 
-app.get('/api/persons', (req, res) => {
-	Phonebook.find({}).then((result) => {
-		res.json(result);
-	});
-});
-
-app.get('/api/persons/:id', (req, res) => {
-	Phonebook.findById(req.params.id)
+app.get('/api/persons', (req, res, next) => {
+	Phonebook.find({})
 		.then((result) => {
 			res.json(result);
 		})
-		.catch((error) => {
-			res.status(404).end();
-		});
+		.catch((error) => next(error));
 });
 
-app.delete('/api/persons/:id', (req, res) => {
-	const id = req.params.id;
-	phoneBook = phoneBook.filter((person) => person.id !== id);
+app.get('/api/persons/:id', (req, res, next) => {
+	Phonebook.findById(req.params.id)
+		.then((phoneBook) => {
+			if (phoneBook) {
+				res.json(phoneBook);
+			} else {
+				res.status(404).end();
+			}
+		})
+		.catch((error) => next(error));
+});
 
-	res.status(204).end();
+app.delete('/api/persons/:id', (req, res, next) => {
+	Phonebook.findByIdAndDelete(req.params.id)
+		.then((result) => {
+			res.status(204).end();
+		})
+		.catch((error) => next(error));
 });
 
 app.post('/api/persons', (req, res) => {
@@ -118,6 +120,36 @@ app.post('/api/persons', (req, res) => {
 		res.json(savedPhonebook);
 	});
 });
+
+app.put('/api/persons/:id', (req, res, next) => {
+	const body = req.body;
+
+	const phoneBook = {
+		name: body.name,
+		number: body.number
+	};
+	Phonebook.findByIdAndUpdate(req.params.id, phoneBook, { new: true })
+		.then((updatedPhonebook) => {
+			res.json(updatedPhonebook);
+		})
+		.catch((error) => next(error));
+});
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint);
+
+// Error handling middleware
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message);
+
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformatted id' });
+	}
+
+	next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
